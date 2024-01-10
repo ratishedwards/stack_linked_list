628060ecb23597f605dde27142f6b3b0366f7cb4
ratishedwards <61689899+ratishedwards@users.noreply.github.com>
2024-01-10 11:34:25 +0530

    Update stack_link_list.h

--- stack_link_list.h
+++ stack_link_list.h
@@ -1,28 +1,46 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"stack_link_list.h"
+#ifndef STACK_LINK_LIST_H
+#define STACK_LINK_LIST_H
 
-int main(){
-	
-	PNODE TOP = NULL;
+struct node {
+	int data;
+	struct node *next;
+};
 
-	push(&TOP, 11);
-	push(&TOP, 12);
-	push(&TOP, 13);
-	push(&TOP, 14);
-	push(&TOP, 15);
-	print(TOP);
+typedef struct node 	NODE;
+typedef struct node *	PNODE;
+typedef struct node **	PPNODE;
 
-	pop(&TOP);
-	pop(&TOP);
-	print(TOP);
-
-	pop(&TOP);
-	pop(&TOP);
-	print(TOP);
-
-	pop(&TOP);
-	print(TOP);
-
-	return 0;
+PNODE	create_node(int data, PNODE next){
+	PNODE new_node = (PNODE)malloc(sizeof(NODE));
+	new_node->data = data;
+	new_node->next = next;
+	return new_node;
 }
+
+void	push(PPNODE top, int data){
+	if (*top == NULL)
+		*top = create_node(data, NULL);
+	else
+		*top = create_node(data, *top);
+}
+
+int	pop(PPNODE top){
+	if(*top){
+		int 	val 	= (*top)->data;
+		PNODE	next	= (*top)->next;
+		free(*top);
+		*top		= next;
+		return val;
+	}
+	return -1;
+}
+
+void	print(PNODE top){
+	while(top){
+		printf(" - %2d ", top->data);
+		top = top->next;
+	}
+	printf("\n");
+}
+
+#endif
